e3c736fb70d8df8e7963af82f130191495eb16cf
Pato Joandet <patriciojoandet@gmail.com>
2022-06-05 23:03:36 -0300

    el pedir por boton ya no se multiplica

--- js/blackjack.js
+++ js/blackjack.js
@@ -12,11 +12,12 @@ let mazo = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,
 let bet = 0;
 let user;
 const users = [];
-let auth = false;
+let auth = true;
 let i = 0;
 let userId = 0;
-let div = document.getElementById("juego");
-
+let div = document.getElementById("botones");
+let cartas = document.getElementById("cartas") 
+let x = 0;
 
 class User{
     constructor(nombre, pass, fichas){
@@ -74,6 +75,12 @@ function recarga(){
 
 
 function repartir(){
+    mano = 0;
+    manoPc = 0;
+    carta1 = 0;
+    carta2 = 0;
+    cartaPc = 0;
+    cartaPc2  = 0;
     mazo = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10];
     carta1 = mazo[Math.floor(Math.random() * mazo.length)];
     pos = mazo.indexOf(carta1);   /// Esto averigua el indice de la carta q salio
@@ -96,172 +103,52 @@ function pedir(){
     carta1 = mazo[Math.floor(Math.random() * mazo.length)];
     pos = mazo.indexOf(carta1);
     mazo.splice(pos, 1);
-    mano += carta1; 
+    mano += carta1;
 }
 
 function quedarse(){
-    alert(`Te quedaste con ${mano}. La Casa tiene ${manoPc}`)
-    if(manoPc<=16 && manoPc<mano){
-        while(manoPc<17){
-            alert("La casa pide una carta")
-            cartaPc = mazo[Math.floor(Math.random() * mazo.length)];
-            alert(`La casa saca ${cartaPc}`)
-            pos = mazo.indexOf(cartaPc);
-            mazo.splice(pos, 1);
-            manoPc+=cartaPc;
-            alert(`La casa tiene ${manoPc}`)
-        }
+    let y = 0
+    cartas.innerHTML = `Te quedaste con ${mano}. La Casa tiene ${manoPc}`
+    for (y = 0;manoPc<17;y++){
+        cartaPc = mazo[Math.floor(Math.random() * mazo.length)];
+        pos = mazo.indexOf(cartaPc);
+        mazo.splice(pos, 1);
+        manoPc+=cartaPc;
     }
+    cartas.innerHTML = `Te quedaste con ${mano}.<br>La casa saca ${y} cartas y se queda con ${manoPc}`
+}
+
+function seguir(){
 
 }
 
 
 function jugar(){
-    if(auth === true){
-        again = true;
-        let x = 0;
-        do{
-            repartir()
-            alert(`Tus cartas son: ${carta1} y ${carta2}. Suman ${mano}. La Casa tiene un ${cartaPc} y una carta oculta.`)
-            if(mano === 21){
-                alert("Blackjack! Ganaste!")
-                won++;
-                x = prompt("Seguir jugando? SI - NO")
-                if(x === "SI"){
-    
-                    again = true;
-                }else if(x === "NO"){
-                    fin()
-                    again = false;
-                }
-            }
-            while(mano!=21 && mano<21){
-                let op = prompt("PEDIR carta o QUEDARSE?");    
-                if(op == "PEDIR"){
-                    pedir();
-                    alert(`Tu carta es un ${carta1} y tu mano vale ${mano}`);
-                    if(mano === 21){
-                        alert("BLACKJACK! Ganaste!")
-                        x = prompt("Seguir jugando? SI - NO")
-                        if(x === "SI"){
-                            again = true;
-                            break;
-                        }else if(x === "NO"){
-                            fin()
-                            again = false;
-                            break;
-                        }
-                    }else if(mano>21){
-                        alert("Te pasaste!");
-                        lost += 1;
-                        x = prompt("Seguir jugando? SI - No")
-                        if(x === "SI"){
-    
-                            again = true;
-                            break;
-                        }else if(x === "NO"){
-                            fin()
-                            again = false;
-                            break;
-                        }   
-                    } 
-                }else if(op === "QUEDARSE"){
-                    alert(`Te quedaste con ${mano}. La Casa tiene ${manoPc}`)
-                    if(manoPc <= 16){
-                        while(manoPc<17){
-                            alert("La casa pide una carta")
-                            cartaPc = mazo[Math.floor(Math.random() * mazo.length)];
-                            pos = mazo.indexOf(cartaPc);
-                            mazo.splice(pos, 1);
-                            manoPc+=cartaPc;
-                            alert(`La casa tiene ${manoPc}`)
-                        }
-                    }   
-                        if(manoPc>21){
-                        alert(`La casa se pasó con ${manoPc}. Ganaste!`)
-                        won++;
-                        x = prompt("Seguir jugando? SI - No")
-                        if(x === "SI"){
-    
-                            again = true;
-                            break;
-                        }else if(x === "NO"){
-                            fin()
-                            again = false;
-                            break;
-                        }   
-                    }               
-                    if(mano === 21){
-                        alert("BLACKJACK! Ganaste!")
-                        x = prompt("Seguir jugando? SI - No")
-                        if(x === "SI"){
-    
-                            again = true;
-                            break;
-                        }else if(x === "NO"){
-                            fin()
-                            again = false;
-                            break;
-                        }
-                    }else if(mano<manoPc){
-                        alert(`La casa gana con ${manoPc}`)
-                        lost++;
-                        x = prompt("Seguir jugando? SI - No")
-                        if(x === "SI"){
-    
-                            again = true;
-                            break;
-                        }else if(x === "NO"){
-                            fin()
-                            again = false;
-                            break;
-                        }
-                    }else if(mano>manoPc){
-                        alert(`Ganaste con ${mano}!`)                
-                        won+=1;
-                        x = prompt("Seguir jugando? SI - No")
-                        if(x === "SI"){
-    
-                            again = true;
-                            break;
-                        }else if(x === "NO"){
-                            fin()
-                            again = false;
-                            break;
-                        }
-                    }else if(mano===manoPc){
-                        alert("Empate");
-                        draw++;
-                        x = prompt("Seguir jugando? SI - No")
-                        if(x === "SI"){
-    
-                            again = true;
-                            break;
-                        }else if(x === "NO"){
-                            fin()
-                            again = false;
-                            break;
-                        }
-                    }
-                }
-            }
-        }while(again==true);
-    }else{
-        alert("Debe iniciar sesion para poder jugar");
+    console.log(mano);
+    btnPedir.disabled=false;
+    mano = 0;
+    manoPc = 0;
+    btnPedir.style.visibility = `visible`;
+    btnQuedar.style.visibility = `visible`;
+    repartir();
+    cartas.innerHTML = `Tus cartas son: ${carta1} y ${carta2}. Tu mano vale ${mano}.<br>
+                            La casa tiene un ${cartaPc} y una carta oculta.`;
+    if(mano === 21){
+        cartas.innerHTML = `Blackjack! Ganaste!`
+        seguir();
+    }else if(mano < 21){
     }
 }
 
-
-function inicio(){
-    alert("Mesa de Blackjack. El objetivo es llegar a 21, o lo mas cercano, sin pasarse. si te pasas, perdes.")
-    jugar();
-}
 
 function fin(){ 
     p.innerHTML =`Victorias: ${won}
     Derrotas: ${lost}
     Empates: ${draw}`
 }
+
+
+
 
 let p = document.getElementById("stats");
 let nav = document.getElementById("nav");
@@ -281,6 +168,30 @@ nav.appendChild(btn1);
 nav.appendChild(btn2);
 nav.appendChild(btn3);
 nav.appendChild(btn4);
+
+let btnPedir = document.createElement("button");
+let btnQuedar = document.createElement("button");
+
+div.appendChild(btnPedir);
+div.appendChild(btnQuedar);
+
+btnPedir.style.visibility = `hidden`
+btnQuedar.style.visibility = `hidden`
+
+btnPedir.innerHTML = "Pedir";
+btnQuedar.innerHTML = "Quedarse";
+
+
+btnPedir.addEventListener("click", () =>{
+    pedir();
+    console.log("hola")
+    cartas.innerHTML = `Sacaste un ${carta1}. Tu mano vale ${mano}.<br>La casa tiene un ${cartaPc} y una carta oculta.`;
+            if(mano>21){
+                cartas.innerHTML = `Te pasaste con ${mano}`
+                lost++
+                seguir()
+            }
+})
 
 btn1.addEventListener("click", () => {
     login();
